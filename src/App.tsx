44630274e40371f54217659e2c265b849321a53f
tristan <heytristaann@gmail.com>
2023-07-13 13:10:45 -0400

    added codeblock and spinner components

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import React, { useEffect, useState } from "react";
+import Spinner from "./components/Spinner";
+import CodeBlock from "./components/CodeBlock";
 import "./App.css";
 
 // Use the github api to get the files from the repo
 // realTristan/tailwindcomponents/contents/tailwindcomponents
 function getFiles(): Promise<any> {
-  let response: any = fetch(
+  return fetch(
     "https://api.github.com/repos/realTristan/tailwindcomponents/contents/tailwindcomponents"
-  );
-  return response.then((res: any) => res.json());
+  ).then((res: any) => res.json());
 }
 
 // Check if the file is valid (ends with .html) (a component)
@@ -15,27 +16,48 @@ function isValidComponent(file_name: string): boolean {
   return file_name.endsWith(".html");
 }
 
+// Read the component from the url
+function readComponent(url: string): Promise<any> {
+  return fetch(url).then((res: any) => res.text());
+}
+
 // Iterate through the files and check if they are valid
-function getComponentsFromFiles(files: any): string[] {
+async function getComponentsFromFiles(files: any): Promise<any> {
   const components: any[] = [];
-  files.forEach((file: any) => {
-    if (isValidComponent(file.name)) {
-      components.push({
-        name: file.name,
-        url: file.download_url,
-        html_url: file.html_url,
-      });
+  for (let i = 0; i < files.length; i++) {
+    const file = files[i];
+    // Check if the file is valid
+    if (!isValidComponent(file.name)) {
+      continue;
     }
-  });
+
+    // State for the component
+    const comp: any = {
+      name: file.name,
+      key: file.sha,
+      html_url: file.html_url,
+      raw_url: file.download_url,
+      code: "",
+    };
+
+    // Read the component
+    await readComponent(file.download_url).then((code) => {
+      components.push({
+        ...comp,
+        code: code,
+      });
+    });
+  }
+
+  // Return the components
   return components;
 }
 
 // Get the components from the files and set the state
 function getComponents(setComps: any) {
-  getFiles().then((files) => {
-    const components: string[] = getComponentsFromFiles(files);
-    setComps(components);
-  });
+  getFiles().then((files) =>
+    getComponentsFromFiles(files).then((comps) => setComps(comps))
+  );
 }
 
 // The main app
@@ -46,16 +68,26 @@ function App() {
   // Get the components
   useEffect(() => getComponents(setComps), []);
 
-  // Render the components
+  // Spinner while waiting for the components and the code to load
+  if (comps.length === 0) {
+    return <Spinner />;
+  }
+
+  // Return the components
   return (
-    <div>
-      {comps.map((comp: any) => {
-        return (
-          <div>
-            <a href={comp.html_url}>{comp.name}</a>
+    <div className="App">
+      <div className="flex flex-col items-center">
+        {comps.map((comp: any) => (
+          <div key={comp.key}>
+            <div className="bg-gray-800 p-4 rounded-lg mt-10 mb-4">
+              <a href={comp.html_url} className="text-lg font-normal text-white">
+                {comp.name}
+              </a>
+            </div>
+            <CodeBlock code={comp.code} />
           </div>
-        );
-      })}
+        ))}
+      </div>
     </div>
   );
 }
---
+++ src/components/Spinner.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+export default class Spinner extends React.Component {
+  render() {
+    return (
+      <div className="flex justify-center items-center h-screen">
+        <svg
+          className="animate-spin -ml-1 mr-3 h-10 w-10 text-slate-900"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+        >
+          <circle
+            className="opacity-25"
+            cx="12"
+            cy="12"
+            r="10"
+            stroke="currentColor"
+            strokeWidth="4"
+          ></circle>
+          <path
+            className="opacity-75"
+            fill="currentColor"
+            d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4z"
+          ></path>
+        </svg>
+      </div>
+    );
+  }
+}
